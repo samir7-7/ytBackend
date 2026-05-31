ae0a3999dd1842d056696138facc97c9323d9b7e
samir7-7 <samirnepal9817@gmail.com>
2026-06-01 02:08:22 +0545

    feat: Add initial implementation of user, video, comment, like, playlist, subscription, and tweet functionalities
    - Created controllers for handling comments, dashboard stats, health checks, likes, playlists, subscriptions, tweets, and user management.
    - Implemented models for comments, likes, playlists, subscriptions, tweets, and users with necessary fields and relationships.
    - Added routes for each functionality with JWT authentication middleware.
    - Introduced utility functions for error handling, API responses, async handling, and Cloudinary uploads.
    - Added a sample .env file for environment variables configuration.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,51 @@
-import dotenv from "dotenv";
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import { app } from "./app.js";
-
+import {app} from './app.js'
 dotenv.config({
-  path: "./env",
-});
+    path: './.env'
+})
+
+
 
 connectDB()
-  .then(() => {
-    app.on("error", (err) => {
-      console.log("ERROR: ", err);
-      throw err;
-    });
-    app.listen(process.env.PORT || 4000, () => {
-      console.log(`Server is running at port ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("Mongodb connection failed: ", err);
-  });
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
+
+
+
+
+
+
+
+
 
 /*
-const app = express();
+import express from "express"
+const app = express()
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
 
-import { DB_NAME } from "./constants.js";
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (err) => {
-      console.log("ERROR: ", err);
-      throw err;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`App listening on port ${process.env.PORT}`);
-    });
-  } catch (err) {
-    console.error("ERROR: ", err);
-  }
-})();
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
 */
